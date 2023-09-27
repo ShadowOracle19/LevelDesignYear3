11e9c34fd96dd1ed46eb6669dff399bec913eaf4
LucasCoates <47534892+ShadowOracle19@users.noreply.github.com>
2023-09-27 18:59:40 -0400

    added it so the game manager tracks the number of targets

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,6 +8,12 @@ public class GameManager : MonoBehaviour
     public List<GameObject> enemyTargets = new List<GameObject>();
     public List<GameObject> civiTargets = new List<GameObject>();
     public float currentTime = 0;
+
+    public TextMeshProUGUI enemyTargetsText;
+    public TextMeshProUGUI civiTargetsText;
+
+    public Transform enemyParent;
+    public Transform civiParent;
 
     private static GameManager _instance;
 
@@ -29,7 +36,17 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Transform target in enemyParent)
+        {
+            enemyTargets.Add(target.gameObject);
+        }
+        foreach (Transform target in civiParent)
+        {
+            civiTargets.Add(target.gameObject);
+        }
+
+        enemyTargetsText.text = $"0/{enemyTargets.Count}";
+        civiTargetsText.text = $"0/{civiTargets.Count}";
     }
 
     // Update is called once per frame
